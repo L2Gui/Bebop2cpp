d3527e87029873e5776b5cd90b7ae257bc3eaf57
Louis Deguillaume <louis.deguillaume@gmail.com>
2017-07-21 12:37:51 +0900

    preparing for experimentation2

--- demo.cpp
+++ demo.cpp
@@ -16,6 +16,17 @@ int interval(int value, int min, int max){
     }
 }
 
+float valueIfAboveEpsilon(float value, float epsilon){
+    if(0 < value and value < epsilon){
+        return 0;
+    }
+    if( -epsilon < value and value < 0){
+        return 0;
+    }
+    return value;
+
+}
+
 int main(){
 
     cv::Point3d wishedPosition(0, -20, 1500);
@@ -32,8 +43,13 @@ int main(){
     }
 
 
-    std::cout << "EVERYTHING IS OK" << std::endl;
+    std::cout << "INITIALISATION IS OK" << std::endl;
 
+
+
+    assert(d.startStreaming());
+
+    std::cout << "STREAMING IS OK" << std::endl;
 
     //d.takeOff();
     //d.land();
@@ -50,8 +66,6 @@ int main(){
 
     assert(d.land());
 */
-
-    assert(d.startStreaming());
 
 /*
     initStream();
@@ -133,6 +147,7 @@ int main(){
 
     cv::Mat tmp;
     cv::Mat frame;
+    bool FOLLOW = false;
     while(true)
     {
         cap >> tmp;
@@ -175,7 +190,7 @@ int main(){
 
                 cv::Mat camPos = rotM.t() * tvec;
                 camPos *= square_size;
-                dist.x = -camPos.at<double>(0,0);
+                dist.x = camPos.at<double>(0,0);
                 dist.y = camPos.at<double>(1,0);
                 dist.z = camPos.at<double>(2,0);
 
@@ -188,15 +203,46 @@ int main(){
                 cv::putText(frame, std::to_string((int)dist.z), cv::Point(10,180), cv::QT_FONT_NORMAL, 1, cv::Scalar(0,0,255), 2, 8);
 
 
+                cv::putText(frame, std::to_string(d.getBatteryLvl()), cv::Point(800, 30),
+                            cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 1, cv::Scalar(255, 0, 255), 3, 8);
 
-                cv::putText(frame, "Dist from wished", cv::Point(10,230), cv::QT_FONT_NORMAL, 1, cv::Scalar(0,0,0), 2, 8);
-                cv::putText(frame, std::to_string((int)distFromWished.x), cv::Point(10,280), cv::QT_FONT_NORMAL, 1, cv::Scalar(255,0,0), 2, 8);
-                cv::putText(frame, std::to_string((int)distFromWished.y), cv::Point(10,330), cv::QT_FONT_NORMAL, 1, cv::Scalar(0,255,0), 2, 8);
-                cv::putText(frame, std::to_string((int)distFromWished.z), cv::Point(10,380), cv::QT_FONT_NORMAL, 1, cv::Scalar(0,0,255), 2, 8);
+                if(FOLLOW) {
+
+                    cv::putText(frame, "Dist from wished", cv::Point(10, 230), cv::QT_FONT_NORMAL, 1,
+                                cv::Scalar(0, 0, 0), 2, 8);
+                    cv::putText(frame, std::to_string((int) distFromWished.x), cv::Point(10, 280), cv::QT_FONT_NORMAL,
+                                1, cv::Scalar(255, 0, 0), 2, 8);
+                    cv::putText(frame, std::to_string((int) distFromWished.y), cv::Point(10, 330), cv::QT_FONT_NORMAL,
+                                1, cv::Scalar(0, 255, 0), 2, 8);
+                    cv::putText(frame, std::to_string((int) distFromWished.z), cv::Point(10, 380), cv::QT_FONT_NORMAL,
+                                1, cv::Scalar(0, 0, 255), 2, 8);
 
 
-                cv::putText(frame, std::to_string(d.getBatteryLvl()), cv::Point(800,30), cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 1, cv::Scalar(255,0,255), 3, 8);
+                    float askedDx = (float) (distFromWished.z / 1000);
+                    float askedDy = -(float) (distFromWished.y / 1000);
+                    float askedDz = -(float) (distFromWished.x / 1000);
 
+                    askedDx = valueIfAboveEpsilon(askedDx, 0.01);
+                    askedDy = valueIfAboveEpsilon(askedDy, 0.01);
+                    askedDz = valueIfAboveEpsilon(askedDz, 0.01);
+
+                    cv::putText(frame, "Asked Dy", cv::Point(100, 280), cv::QT_FONT_NORMAL, 1, cv::Scalar(0, 0, 0), 2,
+                                8); //x
+                    cv::putText(frame, "Asked Dz", cv::Point(100, 330), cv::QT_FONT_NORMAL, 1, cv::Scalar(0, 0, 0), 2,
+                                8); //y
+                    cv::putText(frame, "Asked Dx", cv::Point(100, 380), cv::QT_FONT_NORMAL, 1, cv::Scalar(0, 0, 0), 2,
+                                8); //z
+
+                    cv::putText(frame, std::to_string(askedDy), cv::Point(300, 280), cv::QT_FONT_NORMAL, 1,
+                                cv::Scalar(255, 0, 0), 2, 8);
+                    cv::putText(frame, std::to_string(askedDz), cv::Point(300, 330), cv::QT_FONT_NORMAL, 1,
+                                cv::Scalar(0, 255, 0), 2, 8);
+                    cv::putText(frame, std::to_string(askedDx), cv::Point(300, 380), cv::QT_FONT_NORMAL, 1,
+                                cv::Scalar(0, 0, 255), 2, 8);
+
+                    d.moveBy(askedDx, askedDy, askedDz, 0);
+                }
+                /*
                 int askedRoll = (int)(distFromWished.x/30);
                 int askedPitch = (int)(distFromWished.z/30);
                 int askedAlt = (int)(distFromWished.y/50);
@@ -216,6 +262,7 @@ int main(){
                 d.modifyRoll(askedRoll);
                 d.modifyAltitude(askedAlt);
                 d.modifyPitch(askedPitch);
+                */
 
                 first_time = false;
             }else{
@@ -230,15 +277,33 @@ int main(){
             /**
              * *********************************************************************************************************
              */
+            std::string help("T: takeoff, L: land, F: follow");
+            help.append(FOLLOW ? " (on)" : " (off)");
+            help.append(", Q: emergency");
+            cv::putText(frame, help, cv::Point(10,465), cv::QT_FONT_NORMAL, 1, cv::Scalar(255,255,255), 2);
+            cv::putText(frame, help, cv::Point(10,465), cv::QT_FONT_NORMAL, 1, cv::Scalar(0,0,0), 1);
+
             cv::imshow("frame", frame);
         }
         char k = (char)cv::waitKey(10);
+
         if(k == 'l') {
             break;
-        }if(k == 'q') {
+        }else
+        if(k == 'q') {
             d.emergency();
             break;
+        }else
+        if(k == 'f') {
+            FOLLOW = !FOLLOW;
+            if(!FOLLOW){
+                d.moveBy(0,0,0,0);
+            }
+        }else
+        if(k == 't') {
+            d.takeOff();
         }
+        
     }
 
     d.land();
